d1ebfcc4f6811388c9e519efc29238e7277bb74a
HetRod <rodriguez.hetsy@gmail.com>
2017-01-04 16:10:31 -0400

    a
    a

--- src/vista/Menu.java
+++ src/vista/Menu.java
@@ -408,7 +408,8 @@ public class Menu extends javax.swing.JFrame {
 
     
     private void jMenuItem2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem2ActionPerformed
-        // TODO add your handling code here:
+       InterfazP i = new InterfazP();
+       i.setVisible(true);
     }//GEN-LAST:event_jMenuItem2ActionPerformed
 
     private void jMenuItem3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jMenuItem3ActionPerformed
